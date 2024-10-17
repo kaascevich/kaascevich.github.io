81ce62fa8dce4534e1e8b87017f1137fed21bac9
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2024-10-17 09:42:49 -0400

    remove unused `focus-visible:underline-offset-0`

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -20,7 +20,7 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
     <li className="my-6">
       <a
         href={href}
-        className="inline-block text-lg font-medium text-skin-accent decoration-wavy underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        className="inline-block text-lg font-medium text-skin-accent decoration-wavy underline-offset-4 focus-visible:no-underline"
       >{
         secHeading
           ? <h2 {...headerProps}>{title}</h2>
