f1aa1b99decfc83c51abc53ce31fa3d0f630ee46
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2024-12-16 09:57:28 -0500

    remove unused social icons

--- src/assets/socialIcons.ts
+++ src/assets/socialIcons.ts
@@ -9,39 +9,6 @@ const socialIcons = {
     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
     <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"/>
   </svg>`,
-  Facebook: `<svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="icon-tabler"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3"/>
-  </svg>`,
-  Instagram: `<svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="icon-tabler"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <rect x="4" y="4" width="16" height="16" rx="4"/>
-    <circle cx="12" cy="12" r="3"/>
-    <line x1="16.5" y1="7.5" x2="16.5" y2="7.501"/>
-  </svg>`,
-  LinkedIn: `<svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="icon-tabler"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <rect x="4" y="4" width="16" height="16" rx="2"/>
-    <line x1="8" y1="11" x2="8" y2="16"/>
-    <line x1="8" y1="8" x2="8" y2="8.01"/>
-    <line x1="12" y1="16" x2="12" y2="11"/>
-    <path d="M16 16v-3a2 2 0 0 0 -4 0"/>
-  </svg>`,
   Mail: `<svg
     xmlns="http://www.w3.org/2000/svg"
     class="icon-tabler"
@@ -51,23 +18,6 @@ const socialIcons = {
     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
     <rect x="3" y="5" width="18" height="14" rx="2"/>
     <polyline points="3 7 12 13 21 7"/>
-  </svg>`,
-  Twitch: `<svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="icon-tabler"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  >
-    <path d="M21 2H3v16h5v4l4-4h5l4-4V2zm-10 9V7m5 4V7"/>
-  </svg>`,
-  YouTube: `<svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="icon-tabler"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  >
-    <path d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"/>
-    <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02"/>
   </svg>`,
   WhatsApp: `<svg
     xmlns="http://www.w3.org/2000/svg"
@@ -79,15 +29,6 @@ const socialIcons = {
     <path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9"/>
     <path d="M9 10a0.5 .5 0 0 0 1 0v-1a0.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a0.5 .5 0 0 0 0 -1h-1a0.5 .5 0 0 0 0 1"/>
   </svg>`,
-  Snapchat: `<svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="icon-tabler"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M16.882 7.842a4.882 4.882 0 0 0 -9.764 0c0 4.273 -.213 6.409 -4.118 8.118c2 .882 2 .882 3 3c3 0 4 2 6 2s3 -2 6 -2c1 -2.118 1 -2.118 3 -3c-3.906 -1.709 -4.118 -3.845 -4.118 -8.118zm-13.882 8.119c4 -2.118 4 -4.118 1 -7.118m17 7.118c-4 -2.118 -4 -4.118 -1 -7.118"/>
-  </svg>`,
   Pinterest: `<svg
     xmlns="http://www.w3.org/2000/svg"
     class="icon-tabler"
@@ -98,29 +39,6 @@ const socialIcons = {
     <line x1="8" y1="20" x2="12" y2="11"/>
     <path d="M10.7 14c.437 1.263 1.43 2 2.55 2c2.071 0 3.75 -1.554 3.75 -4a5 5 0 1 0 -9.7 1.7"/>
     <circle cx="12" cy="12" r="9"/>
-  </svg>`,
-  TikTok: `<svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="icon-tabler"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M9 12a4 4 0 1 0 4 4v-12a5 5 0 0 0 5 5"/>
-  </svg>`,
-  CodePen: `<svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="icon-tabler"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M3 15l9 6l9 -6l-9 -6l-9 6"/>
-    <path d="M3 9l9 6l9 -6l-9 -6l-9 6"/>
-    <line x1="3" y1="9" x2="3" y2="15"/>
-    <line x1="21" y1="9" x2="21" y2="15"/>
-    <line x1="12" y1="3" x2="12" y2="9"/>
-    <line x1="12" y1="15" x2="12" y2="21"/>
   </svg>`,
   Discord: `<svg
     xmlns="http://www.w3.org/2000/svg"
@@ -136,15 +54,6 @@ const socialIcons = {
     <path d="M15.5 17c0 1 1.5 3 2 3c1.5 0 2.833 -1.667 3.5 -3c.667 -1.667 .5 -5.833 -1.5 -11.5c-1.457 -1.015 -3 -1.34 -4.5 -1.5l-1 2.5"/>
     <path d="M8.5 17c0 1 -1.356 3 -1.832 3c-1.429 0 -2.698 -1.667 -3.333 -3c-.635 -1.667 -.476 -5.833 1.428 -11.5c1.388 -1.015 2.782 -1.34 4.237 -1.5l1 2.5"/>
   </svg>`,
-  GitLab: `<svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="icon-tabler"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M21 14l-9 7l-9 -7l3 -11l3 7h6l3 -7z"/>
-  </svg>`,
   Reddit: `<svg
     xmlns="http://www.w3.org/2000/svg"
     class="icon-tabler"
@@ -158,16 +67,6 @@ const socialIcons = {
     <circle cx="9" cy="13" r=".5" fill="currentColor"/>
     <circle cx="15" cy="13" r=".5" fill="currentColor"/>
     <path d="M10 17c.667 .333 1.333 .5 2 .5s1.333 -.167 2 -.5"/>
-  </svg>`,
-  Skype: `<svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="icon-tabler"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-  >
-    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-    <path d="M12 3a9 9 0 0 1 8.603 11.65a4.5 4.5 0 0 1 -5.953 5.953a9 9 0 0 1 -11.253 -11.253a4.5 4.5 0 0 1 5.953 -5.954a8.987 8.987 0 0 1 2.65 -.396z"/>
-    <path d="M8 14.5c.5 2 2.358 2.5 4 2.5c2.905 0 4 -1.187 4 -2.5c0 -1.503 -1.927 -2.5 -4 -2.5s-4 -.997 -4 -2.5c0 -1.313 1.095 -2.5 4 -2.5c1.642 0 3.5 .5 4 2.5"/>
   </svg>`,
   Steam: `<svg
     xmlns="http://www.w3.org/2000/svg"
