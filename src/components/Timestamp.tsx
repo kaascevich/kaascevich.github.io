e7d3c8943a6783bd950b453c9a802d1a73024f61
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2024-12-16 10:34:23 -0500

    un-stringify some attributes

--- src/components/Timestamp.tsx
+++ src/components/Timestamp.tsx
@@ -31,7 +31,7 @@ export default function Timestamp({
         className={`${
           size === "sm" ? "scale-90 mr-0.5" : "scale-100 mr-1"
         } inline-block h-7 w-6 min-w-[1.375rem] fill-skin-base`}
-        aria-hidden="true"
+        aria-hidden
       >
         <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"/>
         <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"/>
@@ -63,7 +63,7 @@ const FormattedTimestamp = ({ published, modified }: TimestampProps) => {
 
   return <span>
     <time dateTime={actualDate.toISOString()}>{date}</time>
-    <span aria-hidden="true"> | </span>
+    <span aria-hidden> | </span>
     <span className="sr-only">&nbsp;at&nbsp;</span>
     <span className="text-nowrap">{time}</span>
   </span>;
