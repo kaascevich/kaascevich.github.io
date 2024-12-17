6a5eff4d1f723e0b23c89cba91977886680331d5
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2024-12-17 12:47:04 -0500

    turn on top-level name mangling

--- astro.config.ts
+++ astro.config.ts
@@ -43,6 +43,11 @@ export default defineConfig({
           conservativeCollapse: true,
         },
       },
+      JavaScript: {
+        "terser": {
+          toplevel: true,
+        },
+      },
     }),
   ],
   markdown: {
