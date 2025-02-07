b98c558565711f44c00992c2ab8efc710b9b7b00
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2025-02-07 09:02:52 -0500

    make `Site` and `Social` types `readonly`

--- src/types.ts
+++ src/types.ts
@@ -3,34 +3,34 @@ import type { CollectionEntry, CollectionKey } from "astro:content"
 /** Global configuration for the site. */
 export interface Site {
   /** The site's URL. */
-  url: URL
+  readonly url: URL
   /** The site's author. */
-  author: string
+  readonly author: string
   /** The author's profile page. */
-  profile: URL
+  readonly profile: URL
   /** The site's description. */
-  description: string
+  readonly description: string
   /** The site's title. */
-  title: string
+  readonly title: string
   /** The number of posts to show on the main page. */
-  postsPerIndex: number
+  readonly postsPerIndex: number
   /** The number of posts to show on each page. */
-  postsPerPage: number
+  readonly postsPerPage: number
   /**
    * The number of minutes before a post's publish date when the post should
    * become visible.
    */
-  scheduledPostMargin: number
+  readonly scheduledPostMargin: number
 }
 
 /** A social media service. */
 export interface Social {
   /** The social media service's name. */
-  name: string
+  readonly name: string
   /** The link's title. */
-  title: string
+  readonly title: string
   /** The URL to the social media service. */
-  href: string
+  readonly href: string
 }
 
 /** A date-time string parsable by Dayjs. */
