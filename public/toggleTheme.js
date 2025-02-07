0cbc7b67dbe2115b944f74c4aea4878f7e3c292f
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2025-02-07 10:16:07 -0500

    reorganize `toggleTheme.js`

--- public/toggleTheme.js
+++ public/toggleTheme.js
@@ -1,65 +1,59 @@
 "use strict"
 
-/** @typedef {"light" | "dark" | null} Theme */
+/** @typedef {"light" | "dark"} Theme A light or dark theme. */
 
-/**
- * Gets the current site theme.
- * @returns {Theme} The current site theme.
-*/
-function getTheme() {
-  return localStorage.getItem("theme")
-}
-
-/**
- * Sets the current site theme.
- * @param {Theme} theme - The new site theme.
- */
-function setTheme(theme) {
-  localStorage.setItem("theme", theme)
-  applyTheme()
-}
-
-/** Applies the current theme to the site. */
-function applyTheme() {
-  document.documentElement.dataset.theme = getTheme() ?? undefined
-  document.getElementById("theme-button")?.setAttribute(
-    "title",
-    `current theme: ${getTheme()}`,
-  )
-
-  if (document.body) {
-    const backgroundColor = getComputedStyle(document.body).backgroundColor
-    document.querySelector("meta[name='theme-color']")?.setAttribute(
-      "content",
-      backgroundColor,
+/** A wrapper for managing the site's theme. */
+const theme = {
+  /** The current site theme. @type {Theme} */
+  get current() {
+    const theme = localStorage.getItem("theme")
+    return theme === "light" || theme === "dark"
+      ? theme
+      : matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
+  },
+  
+  set current(theme) {
+    localStorage.setItem("theme", theme)
+    this.apply()
+  },
+  
+  /** Applies the current theme to the site. */
+  apply() {
+    document.documentElement.dataset.theme = this.current
+    document.getElementById("theme-button")?.setAttribute(
+      "title",
+      `current theme: ${this.current}`,
     )
-  }
-}
 
-if (getTheme() === null) {
-  setTheme(
-    matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light",
-  )
+    if (document.body) {
+      const backgroundColor = getComputedStyle(document.body).backgroundColor
+      document.querySelector("meta[name='theme-color']")?.setAttribute(
+        "content",
+        backgroundColor,
+      )
+    }
+  }
 }
 
 // set early to avoid flashes
-applyTheme()
+theme.apply()
 
 onload = () => {
-  function setUpThemes() {
-    applyTheme()
+  function setUpTheme() {
+    theme.apply()
 
     document.getElementById("theme-button")?.addEventListener(
-      "click", () => setTheme(getTheme() === "light" ? "dark" : "light"),
+      "click",
+      () => theme.current = theme.current === "light" ? "dark" : "light",
     )
   }
 
-  setUpThemes()
-  document.addEventListener("astro:after-swap", setUpThemes)
+  setUpTheme()
+  document.addEventListener("astro:after-swap", setUpTheme)
 }
 
 // sync with system changes
 matchMedia("(prefers-color-scheme: dark)").addEventListener(
   "change",
-  ({ matches: isDark }) => setTheme(isDark ? "dark" : "light"),
+  ({ matches: isDark }) => theme.current = isDark ? "dark" : "light",
 )
