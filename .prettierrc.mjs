b1e24eb7564fb288fd8c51c66c6e57117459bf73
Kaleb Ascevich <swimmer-flowing-0s@icloud.com>
2025-02-14 13:39:15 -0500

    add svelte formatting

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,11 +1,9 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-svelte"],
   overrides: [
-    {
-      files: "*.astro",
-      options: { parser: "astro" },
-    },
+    { files: "*.astro", options: { parser: "astro" } },
+    { files: "*.svelte", options: { parser: "svelte" } },
   ],
 
   experimentalTernaries: true,
