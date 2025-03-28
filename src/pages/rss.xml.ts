a5aef9d912703a500b0e0399392cca7721652caf
Kaleb Ascevich <swimmer-flowing-0s@icloud.com>
2025-03-28 09:00:10 -0400

    fix invalid characters in RSS

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -12,19 +12,33 @@ const parser = new MarkdownIt()
 export async function GET(context: APIContext) {
   const posts = await getSortedPosts()
 
-  return rss({
-    title: siteConfig.title,
-    description: siteConfig.subtitle,
-    site: context.site ?? siteConfig.url,
-    items: posts.map((post) => ({
+  const items = posts.map((post) => {
+    const content = sanitizeHtml(parser.render(post.body!), {
+      allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+    })
+
+    // if (post.data.title === 'Aurora (part 2)') {
+    //   console.log(content.replace(/\[2m/, ''))
+    // }
+
+    // filter out ANSI escapes from code blocks
+    // eslint-disable-next-line no-control-regex
+    const filteredContent = content.replaceAll(/\x1B\[\d+(?:;\d+)*m/g, '')
+
+    return {
       title: post.data.title,
       pubDate: post.data.published,
       description: post.data.description,
       link: getPostUrlByID(post.id),
       categories: post.data.tags as string[],
-      content: sanitizeHtml(parser.render(post.body!), {
-        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
-      }),
-    })),
+      content: filteredContent,
+    }
+  })
+
+  return rss({
+    title: siteConfig.title,
+    description: siteConfig.subtitle,
+    site: context.site ?? siteConfig.url,
+    items,
   })
 }
